c3d5a7f4901dcc845b7ad454f52b06a95564db21
cyeniceri <cem.yeniceri@gmail.com>
2025-09-27 23:59:46 +0100

    Spring WebClient with Oauth2 Client Configuration

---
+++ src/main/java/guru/springframework/spring6webclient/client/BeerClient.java
@@ -0,0 +1,31 @@
+package guru.springframework.spring6webclient.client;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import guru.springframework.spring6webclient.model.BeerDTO;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+public interface BeerClient {
+
+    Flux<String> listBeer();
+
+    Flux<Map> listBeerMap();
+
+    Flux<JsonNode> listBeersJsonNode();
+
+    Flux<BeerDTO> listBeerDtos();
+
+    Mono<BeerDTO> getBeerById(String id);
+
+    Flux<BeerDTO> getBeerByBeerStyle(String beerStyle);
+
+    Mono<BeerDTO> createBeer(BeerDTO beerDTO);
+
+    Mono<BeerDTO> updateBeer(BeerDTO beerDTO);
+
+    Mono<Void> deleteBeer(String id);
+
+    Mono<BeerDTO> patchBeer(BeerDTO beerDTO);
+}
